601f0a1275ca6c8f8137c0aa66b7463f73575b04
Victor Akachukwu <victorakachukwu19@gmail.com>
2022-09-21 14:42:39 +0000

    convertin letters properly

---
+++ c_programing/chapter_3/convert_letters.c
@@ -0,0 +1,19 @@
+/*program 3.5 testing letters the easy way*/
+#include <stdio.h>
+
+int main(void)
+{
+	char letter = 0;
+
+	printf("Enter an uppercase letter:"); /*promp for input*/
+	scanf("%c", &letter);  /*read the input character*/
+
+	if((letter >= 'A') && (letter <= 'Z')) /*verify uppercase letter*/
+	{
+		letter += 'a' - 'A';   /*convert to lowercase*/
+		printf("You entered an uppercase %c \n");
+	}
+	else
+		printf("You did not enter an uppercase letter\n");
+	return 0;
+}
